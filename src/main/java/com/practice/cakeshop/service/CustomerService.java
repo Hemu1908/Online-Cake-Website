dd5a4c6e79f9bc5a29ecc81e0c7f65a56672dab2
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-05-03 20:39:44 +0530

    Added login API

--- src/main/java/com/practice/cakeshop/service/CustomerService.java
+++ src/main/java/com/practice/cakeshop/service/CustomerService.java
@@ -1,9 +1,11 @@
 package com.practice.cakeshop.service;
 
+import com.practice.cakeshop.dto.LoginStatus;
 import com.practice.cakeshop.entity.Customer;
 
 public interface CustomerService {
 	
 	public Customer register(Customer customer);
+	public LoginStatus login(String emailId, String password);
 
 }
--- src/main/java/com/practice/cakeshop/service/CustomerServiceImpl.java
+++ src/main/java/com/practice/cakeshop/service/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.practice.cakeshop.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.practice.cakeshop.dto.LoginStatus;
 import com.practice.cakeshop.entity.Customer;
 import com.practice.cakeshop.repository.CustomerRepository;
 
@@ -16,6 +17,12 @@ public class CustomerServiceImpl implements CustomerService{
 	public Customer register(Customer customer) {
 		// TODO Auto-generated method stub
 		return custRepo.register(customer);
+	}
+
+	@Override
+	public LoginStatus login(String emailId, String password) {
+		// TODO Auto-generated method stub
+		return custRepo.login(emailId, password);
 	}
 
 }
