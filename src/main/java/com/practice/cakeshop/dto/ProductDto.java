0e84d53805d586fc069ace82cc29ccecfd156000
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-05-05 12:39:01 +0530

    category and product api

--- src/main/java/com/practice/cakeshop/dto/ProductDto.java
+++ src/main/java/com/practice/cakeshop/dto/ProductDto.java
@@ -1,86 +1,55 @@
-package com.practice.cakeshop.entity;
+package com.practice.cakeshop.dto;
 
 import javax.mail.Multipart;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "shop_product")
-public class Product {
+import com.practice.cakeshop.entity.Category;
+
+public class ProductDto {
 	
-	@Id
-	@SequenceGenerator(name="customer_seq", initialValue=1, allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="product_seq")
-	@Column(name = "product_id")
 	int productId;
-	
-	@Column(unique=true)
 	String name;
 	String description;
 	double unitPrice;
 	Multipart image;
+	Category category;
 	
-	@ManyToOne 
-	@JoinColumn(name = "category_id")
-	private Category category;
-
 	public int getProductId() {
 		return productId;
 	}
-
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 	public double getUnitPrice() {
 		return unitPrice;
 	}
-
 	public void setUnitPrice(double unitPrice) {
 		this.unitPrice = unitPrice;
 	}
-
 	public Multipart getImage() {
 		return image;
 	}
-
 	public void setImage(Multipart image) {
 		this.image = image;
 	}
-
 	public Category getCategory() {
 		return category;
 	}
-
 	public void setCategory(Category category) {
 		this.category = category;
 	}
 	
 	
 	
-	
-
 }
--- src/main/java/com/practice/cakeshop/repository/CustomerRepository.java
+++ src/main/java/com/practice/cakeshop/repository/CustomerRepository.java
@@ -1,10 +1,19 @@
 package com.practice.cakeshop.repository;
 
+import com.practice.cakeshop.dto.CategoryDto;
 import com.practice.cakeshop.dto.LoginStatus;
+import com.practice.cakeshop.dto.ProductDto;
+import com.practice.cakeshop.entity.Category;
 import com.practice.cakeshop.entity.Customer;
+import com.practice.cakeshop.entity.Product;
 
 public interface CustomerRepository {
 	
 	public Customer register(Customer customer);
 	public LoginStatus login(String email, String password);
+	public boolean isProductPresent(String name);
+	public Product addProduct(ProductDto productDto);
+	public boolean isCategoryPresent(String name);
+	public Category addCategory(CategoryDto categoryDto);
+	public Category findCategoryByName(String name);
 }
