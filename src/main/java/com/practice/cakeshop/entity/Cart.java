cee37ee631a0fbbd2a9eb4a941f04cbe8a504322
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-05-06 14:47:11 +0530

    created cart and cartItem table with mapping

---
+++ src/main/java/com/practice/cakeshop/entity/Cart.java
@@ -0,0 +1,59 @@
+package com.practice.cakeshop.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shop_cart")
+public class Cart {
+	
+	@Id
+	@SequenceGenerator(name="cart_seq", initialValue=101, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="cart_seq")
+	@Column(name = "cart_id")
+	int cartId;
+	
+	@OneToOne 
+	@JoinColumn(name = "customer_id")
+	Customer customer;
+	
+	@OneToMany(cascade = CascadeType.MERGE, mappedBy = "cart")
+	private List<CartItem> cartItems;
+
+	public int getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<CartItem> getCartItems() {
+		return cartItems;
+	}
+
+	public void setCartItems(List<CartItem> cartItems) {
+		this.cartItems = cartItems;
+	}
+
+	
+}
---
+++ src/main/java/com/practice/cakeshop/entity/CartItem.java
@@ -0,0 +1,66 @@
+package com.practice.cakeshop.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shop_cartItem")
+public class CartItem {
+	
+	@Id
+	@SequenceGenerator(name="cartItem_seq", initialValue=101, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="cartItem_seq")
+	@Column(name = "cartItem_id")
+	int cartItemId;
+	
+	@ManyToOne 
+	@JoinColumn(name = "cart_id")
+	private Cart cart;
+	
+	@OneToOne 
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	int quantity;
+
+	public int getCartItemId() {
+		return cartItemId;
+	}
+
+	public void setCartItemId(int cartItemId) {
+		this.cartItemId = cartItemId;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
